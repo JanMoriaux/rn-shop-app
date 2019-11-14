21980050e967778f89a3595fa33f3a084fbe3028
Jan Moriaux <janmoriaux1@gmail.com>
2019-11-14 21:07:38 +0100

    161 complete

---
+++ screens/Template.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+const Template = props => {
+  return (
+    <View>
+      <Text>Template</Text>
+    </View>
+  );
+};
+
+export default Template;
+
+const styles = StyleSheet.create({});
